bebc7308ed05cb019660399bc71d8bda62d6ded4
Viriato Sampaio <viriatosampaio@gmail.com>
2023-08-13 16:15:03 -0300

    add base path

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://viriatonova.github.io',
+  base: '/viriatonova',
   integrations: [mdx(), sitemap(), tailwind()]
 });
