469d37b9ba4f928818dea0f5f851eccfd129fb29
Viriato Nova <viriatosampaio@gmail.com>
2023-08-19 22:58:18 -0300

    updating

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Noto Sans', 'sans-serif'],
+			mono: ['Noto Sans Mono', 'monospace'],
+		},
 		extend: {},
 	},
 	plugins: [],
